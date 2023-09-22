316eb198685f23af4e8b67d3dabfde1d1c838d8f
iLestar <juanlestar0408@gmail.com>
2023-09-22 08:47:27 -0500

    v0.3.0 delete todolist works

--- src/components/card.vue
+++ src/components/card.vue
@@ -1,9 +1,9 @@
 <script lang="ts">
-import { IconCheck, IconSend } from '@tabler/icons-vue'
+import { IconCheck, IconSend, IconEraser } from '@tabler/icons-vue'
 import { ref, onMounted } from 'vue'
 
 export default {
-  components: { IconSend, IconCheck },
+  components: { IconSend, IconCheck, IconEraser },
   setup() {
     const todoList = ref([] as string[])
     const doneList = ref([] as string[])
@@ -49,6 +49,15 @@ export default {
       this.doneList.push(find)
       localStorage.setItem('doneList', JSON.stringify(Array.from(this.doneList)))
     },
+    deleteTodo(index: number, listType: string) {
+      if (listType === 'incomplete') {
+        this.todoList.splice(index, 1)
+        localStorage.setItem('todoList', JSON.stringify(Array.from(this.todoList)))
+      } else if (listType === 'complete') {
+        this.doneList.splice(index, 1)
+        localStorage.setItem('doneList', JSON.stringify(Array.from(this.doneList)))
+      }
+    },
   },
 }
 </script>
@@ -63,7 +72,7 @@ export default {
       <form class="flex flex-col gap-1" v-on:submit="handleSubmit">
         <label class="text-sm text-gray-300" for="todo">Crea una tarea aquí</label>
         <section class="relative">
-          <input class="rounded p-2 focus:placeholder-transparent focus:outline-none w-full" placeholder="Hacer el mercado..." type="text" name="todo" id="todo" autocomplete="off" />
+          <input class="rounded p-2 focus:placeholder-transparent focus:outline-none w-full" placeholder="ej: Hacer el mercado..." type="text" name="todo" id="todo" autocomplete="off" required />
           <button class="absolute right-3 bottom-2"><IconSend /></button>
         </section>
       </form>
@@ -73,15 +82,22 @@ export default {
           <section class="min-h[3rem] max-h-72 overflow-y-auto flex flex-col">
             <div v-for="(item, index) in todoList" :key="index" class="flex justify-between pr-4">
               <span> {{ `${index + 1}. ${item}` }} </span>
-              <button type="button" @click="changeStateTodo(index)"><IconCheck color="#2fb344" /></button>
+              <div>
+                <button type="button" @click="changeStateTodo(index)"><IconCheck color="#2fb344" /></button>
+                <button type="button" @click="deleteTodo(index, 'incomplete')"><IconEraser color="#FF0000" /></button>
+              </div>
             </div>
           </section>
         </section>
         <section>
           <h2>Tareas completas</h2>
           <section class="min-h[3rem] max-h-72 overflow-y-auto flex flex-col">
-            <div v-for="(item, index) in doneList" :key="index" class="">
+            <div v-if="doneList.length === 0">
+              <span>No hay tareas completas aún</span>
+            </div>
+            <div v-else v-for="(item, index) in doneList" :key="index" class="flex justify-between pr-4">
               <span>{{ `${index + 1}. ${item}` }}</span>
+              <button type="button" @click="deleteTodo(index, 'complete')"><IconEraser color="#FF0000" /></button>
             </div>
           </section>
         </section>
